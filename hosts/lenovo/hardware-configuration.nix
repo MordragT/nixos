3d44e1a25f4b873adb542a80ba8b87197df055ee
Thomas Wehmöller <connect.mordrag@gmx.de>
2023-02-03 01:43:28 +0100

    feat: Add swapfile for lenovo

--- hosts/lenovo/hardware-configuration.nix
+++ hosts/lenovo/hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_usb_sdmmc" ];
@@ -14,21 +15,24 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/29a924dc-05e9-4009-a9c7-f18059cbe6af";
+    {
+      device = "/dev/disk/by-uuid/29a924dc-05e9-4009-a9c7-f18059cbe6af";
       fsType = "f2fs";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/6279-C816";
+    {
+      device = "/dev/disk/by-uuid/6279-C816";
       fsType = "vfat";
     };
 
   fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/1c993eac-9840-4539-8639-2c91454ff5a4";
+    {
+      device = "/dev/disk/by-uuid/1c993eac-9840-4539-8639-2c91454ff5a4";
       fsType = "btrfs";
     };
 
-  swapDevices = [ ];
+  swapDevices = [{ device = "/swapfile"; size = 4096; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
