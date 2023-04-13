70eb675ec029c4e7a8008b71caf983d2aa20d025
Thomas Wehmöller <connect.mordrag@gmx.de>
2023-04-13 20:56:01 +0200

    ✨ Add corectrl program

--- system/programs/default.nix
+++ system/programs/default.nix
@@ -2,6 +2,7 @@
 {
   programs.adb.enable = true;
   programs.bandwhich.enable = true; # view network utilization
+  programs.corectrl.enable = true;
   programs.droidcam.enable = true;
   programs.gamemode.enable = true;
   programs.wireshark.enable = true;
