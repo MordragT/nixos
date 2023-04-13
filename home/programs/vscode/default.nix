e8915d31efd7f0b5c82a3b4d1a38dfe10aa3f738
Thomas Wehmöller <connect.mordrag@gmx.de>
2023-04-13 16:05:45 +0200

    fix: Fix vscode platformio hash

--- home/programs/vscode/default.nix
+++ home/programs/vscode/default.nix
@@ -274,7 +274,7 @@ in
         name = "platformio-ide";
         publisher = "platformio";
         version = "3.1.1";
-        sha256 = "1j2jpj9pw9qp5gzqrgajck6hnshr94kcss5l739vgwg3njviq0bz";
+        sha256 = "g9yTG3DjVUS2w9eHGAai5LoIfEGus+FPhqDnCi4e90Q=";
       }
     ];
   };
