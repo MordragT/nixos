8d63620afaae5f94b68d2160d69dad83c5398318
Thomas Wehmöller <connect.mordrag@gmx.de>
2023-04-30 20:00:29 +0200

    🐛 Fix home manager target for non nixos

--- home/programs/vscode/default.nix
+++ home/programs/vscode/default.nix
@@ -231,7 +231,7 @@ in
         name = "platformio-ide";
         publisher = "platformio";
         version = "3.1.1";
-        sha256 = "g9yTG3DjVUS2w9eHGAai5LoIfEGus+FPhqDnCi4e90Q=";
+        sha256 = "fwEct7Tj8bfTOLRozSZJGWoLzWRSvYz/KxcnfpO8Usg=";
       }
       {
         name = "sublime-keybindings";
