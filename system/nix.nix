01c56514f8ca58b811599a5dc9c5baac257f64b5
Thomas Wehmöller <connect.mordrag@gmx.de>
2023-05-19 22:51:00 +0200

    🐛 Fix nix substituters

--- system/nix.nix
+++ system/nix.nix
@@ -1,6 +1,15 @@
 { ... }:
 {
   nix = {
+    settings = {
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
     gc = {
       automatic = true;
       options = "--delete-older-than 7d";
