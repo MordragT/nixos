0de21d425ed8de1c7c170127af5aa57dbdf9756b
Thomas Wehmöller <connect.mordrag@gmx.de>
2024-12-11 18:23:05 +0100

    🐛 Use verified version of triton

--- pkgs/by-scope/intel-python/triton-xpu/default.nix
+++ pkgs/by-scope/intel-python/triton-xpu/default.nix
@@ -6,15 +6,15 @@
 }:
 buildPythonPackage rec {
   pname = "pytorch_triton_xpu";
-  version = "3.2.0";
+  version = "3.0.0";
   format = "wheel";
 
   src = fetchwheel {
     base = "https://download.pytorch.org/";
     dist = "whl/nightly";
-    abi = "%2Bgite98b6fcb";
+    abi = "%2B1b2f15840e";
     package = "${pname}-${version}";
-    sha256 = "sha256-N2+42RaRWSXKz5iGmHfoMu7PgqkeOj2e709lpusJofA=";
+    sha256 = "sha256-hwFG6wzEiaSam7mAh6xl30XF+TENGOadY5mgSIEY7D4=";
   };
 
   nativeBuildInputs = [
